dbbc499f93818b233152002688dca078ee96e31a
mahesh-gouda <maheshgouda133@gmail.com>
2022-10-15 13:04:10 +0530

    Pos printer option implemented

---
+++ app/src/main/java/com/devappsys/posprinter/MainActivity.java
@@ -0,0 +1,66 @@
+package com.devappsys.posprinter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.cloudpos.DeviceException;
+import com.cloudpos.POSTerminal;
+import com.cloudpos.printer.PrinterDevice;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    private PrinterDevice device = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnPRint = findViewById(R.id.btnPrint);
+        btnPRint.setOnClickListener(view -> {
+            printData();
+        });
+
+    }
+
+
+    private void printData() {
+        //Getting the POS printerDevice object reference
+        if (device == null) {
+            device = (PrinterDevice) POSTerminal.getInstance(this)
+                    .getDevice("cloudpos.device.printer");
+        }
+
+        //Opening the Printer device for printing,
+        try {
+            device.open();
+        } catch (DeviceException e) {
+            e.printStackTrace();
+        }
+
+        //Once the device is open we can send our text to print
+        try {
+            device.printText(
+                    "\n---DevAppSys---\n" +
+                            "MERCHANT COPY\n" +
+                            "" +
+                            "MERCHANT NAME\n" +
+                            "SHXXXXXXCo.,LTD.\n" +
+                            "530310041315039\n" +
+                            "TERMINAL NO\n" +
+                            "50000045" +
+                            "OPERATOR\n" +
+                            "50000045" +
+                            "\n" +
+                            "\n");
+
+        } catch (DeviceException e) {
+            e.printStackTrace();
+
+        }
+    }
+
+}
+
